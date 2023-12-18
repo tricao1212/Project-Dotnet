86de07fa695aa6b46fb5cc880531f33e7bb55525
phucdang2002 <phuc.dang.cit20@eiu.edu.vn>
2023-12-18 16:51:29 +0700

    design Home page

---
+++ BookStore/BookStore/Models/PagingInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
