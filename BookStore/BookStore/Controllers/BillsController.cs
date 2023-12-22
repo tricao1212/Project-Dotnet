e4381b65e78414e46b3a392c4c9772f7b295054c
phucdang2002 <phuc.dang.cit20@eiu.edu.vn>
2023-12-22 08:10:26 +0700

    Add to cart

---
+++ BookStore/BookStore/Controllers/BillsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class BillsController : Controller
+    {
+        private readonly BookStoreContext _context;
+
+        public BillsController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bills
+        public async Task<IActionResult> Index()
+        {
+            var bookStoreContext = _context.Bill.Include(b => b.User);
+            return View(await bookStoreContext.ToListAsync());
+        }
+
+        // GET: Bills/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Bill == null)
+            {
+                return NotFound();
+            }
+
+            var bill = await _context.Bill
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bill == null)
+            {
+                return NotFound();
+            }
+
+            return View(bill);
+        }
+
+        // GET: Bills/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Bills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CreatedDate,Payment,Quantity,Address,Phone,Note,UserId,Status")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bill);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", bill.UserId);
+            return View(bill);
+        }
+
+        // GET: Bills/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Bill == null)
+            {
+                return NotFound();
+            }
+
+            var bill = await _context.Bill.FindAsync(id);
+            if (bill == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", bill.UserId);
+            return View(bill);
+        }
+
+        // POST: Bills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CreatedDate,Payment,Quantity,Address,Phone,Note,UserId,Status")] Bill bill)
+        {
+            if (id != bill.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bill);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BillExists(bill.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", bill.UserId);
+            return View(bill);
+        }
+
+        // GET: Bills/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Bill == null)
+            {
+                return NotFound();
+            }
+
+            var bill = await _context.Bill
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bill == null)
+            {
+                return NotFound();
+            }
+
+            return View(bill);
+        }
+
+        // POST: Bills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Bill == null)
+            {
+                return Problem("Entity set 'BookStoreContext.Bill'  is null.");
+            }
+            var bill = await _context.Bill.FindAsync(id);
+            if (bill != null)
+            {
+                _context.Bill.Remove(bill);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BillExists(int id)
+        {
+          return _context.Bill.Any(e => e.Id == id);
+        }
+    }
+}
